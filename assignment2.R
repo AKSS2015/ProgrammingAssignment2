2b738cfe30a13150aebb8959c414f8c314871b99
AKSS2015 <akss.blessed@gmail.com>
2015-07-26 19:00:35 +0530

    Adding my FINAL file having code for Assignment2 thru this commit.

--- assignment2.R
+++ assignment2.R
@@ -1,25 +1,33 @@
-makeCacheMatrix <- function(x = matrix()){
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function (solve) m <<- solve(x)
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-}
+makeCacheMatrix <- function(x = matrix())
+        { # This function creates a special object that stores a "matrix"
+        m <- NULL
+                set <- function(y) {
+                x <<- y
+                m <<- NULL
+                }
+                get <- function() x
+                setinverse <- function (solve) m <<- solve(x)
+                getinverse <- function() m
+                list(set = set, get = get,
+                setinverse = setinverse,
+                getinverse = getinverse) #the special object created by 
+                # makeCacheMatrix is a list containing a function to set
+                # and get the value of the matrix and set and get the 
+                # value of its inverse
+        }
 
-cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached inverse")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
-}
+cacheSolve <- function(x, ...) 
+        { # This function calculates the inverse of the
+        # special "matrix" object created with makeCacheMatrix.
+        m <- x$getinverse()
+        if(!is.null(m)) # Checks if the inverse's already been calculated
+                { 
+                message("getting cached inverse")
+                return(m) # Returns the cached inverse
+                }
+        data <- x$get()
+        m <- solve(data, ...)# calculates the inverse
+        x$setinverse(m) # Sets the value of the inverse in the cache via
+        # the setinverse function
+        m
+        }
